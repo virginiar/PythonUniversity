eb4921d662ae80e0ed492aa41daa922eb76b219a
Virginia Redondo <7237305+virginiar@users.noreply.github.com>
2026-03-05 11:34:14 +0100

    Introducción a Python

---
+++ 01-Introduccion/hola_mundo.py
@@ -0,0 +1,3 @@
+# Programa Hola Mundo con Python
+print("¡Hola Mundo!") # Primera línea de código
+print("Saludos...") # Segunda línea de código
---
+++ 01-Introduccion/presentacion.py
@@ -0,0 +1,4 @@
+# Ejercicio de Presentación
+print("Nombre: Juan Pérez")
+print("Edad: 28")
+print("País: México")
